ab4d5f8cb00e28d1960357705e65fac99ba987fa
EllieShilla <nosova.alina97@gmail.com>
2021-05-19 20:44:03 +0300

    11
    1

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function Name()
+{
+  return <p>Нил Гейман</p>;
+}
+
+
+export default function App()
+{
+  return(
+    <div class="text">
+     <h1>Нико-где</h1>
+     <Name/>
+    </div>
+  )
+}
